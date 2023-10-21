ec143590901eb63b34b3ba98cf883d686876ae66
DamiMedrano <damian.e.medrano@hotmail.com>
2023-10-21 13:15:02 -0300

    ImageGallery done

--- src/App.js
+++ src/App.js
@@ -8,23 +8,37 @@ import { TeamsList } from "./components/TeamsList";
 import './App.css';
 
 export default function App() {
+
+  const imageLinks = [
+    "https://i.redd.it/funkib13kvf31.jpg",
+    "https://i.redd.it/1o219x45sz771.jpg",
+    "https://i.redd.it/awamt0irl5h41.jpg",
+    "https://i.redd.it/0rypgwdi1xe31.jpg",
+  ];
+
   return (
     <div className="App">
       {/* Render here each component from the "components" directory */}
-      <h3>'Message' test</h3>
-      <Message />
-      <br />
-      <h3>'FocusableInput' test</h3>
-      <FocusableInput />
-      <br />
-      <h3>'ImageGallery' test</h3>
-      <ImageGallery />
-      <br />
-      <h3>'PlayerStatus' test</h3>
-      <PlayerStatus />
-      <br />
-      <h3>'TeamsList' test</h3>
-      <TeamsList />
+      <div className="test-container">
+        <h3>'Message' test</h3>
+        <Message />
+      </div>
+      <div className="test-container">
+        <h3>'FocusableInput' test</h3>
+        <FocusableInput focused={false} />
+      </div>
+      <div className="test-container">
+        <h3>'ImageGallery' test</h3>
+        <ImageGallery links={imageLinks}/>
+      </div>
+      <div className="test-container">
+        <h3>'PlayerStatus' test</h3>
+        <PlayerStatus />
+      </div>
+      <div className="test-container">
+        <h3>'TeamsList' test</h3>
+        <TeamsList />
+      </div>
     </div>
   );
 }
--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,25 +1,39 @@
 /**
  * Implement the ImageGallery component that accepts a `links`
- * prop and renders the gallery so that the first
+ * prop and renders the gallery so that the first 
  * item in the links prop is the src attribute of the first image in the gallery.
 
  * It should also implement the following logic:
  * - When the button is clicked, the image that is in the same div as the button should be removed from the gallery.
  */
 
+import React, { useState } from "react";
+
 function Image({ src, onRemove }) {
-	return (
-		<div class="image">
-			<img />
-			<button class="remove">X</button>
-		</div>
-	);
+  return (
+    <div className="image">
+      <img className="thumbnail" src={src} alt="" />
+      <button className="remove-button" onClick={onRemove}>
+        x
+      </button>
+    </div>
+  );
 }
 
-export function ImageGallery({ links }) {
-	return (
-		<div>
-			{/* Implement here the Image Gallery using <Image /> component */}
-		</div>
-	);
+export function ImageGallery({ links = [] }) {
+  const [images, setImages] = useState(links);
+
+  const handleRemove = (index) => {
+    const updatedImages = [...images];
+    updatedImages.splice(index, 1);
+    setImages(updatedImages);
+  };
+
+  return (
+    <div className="image-gallery">
+      {images.map((link, index) => (
+        <Image key={index} src={link} onRemove={() => handleRemove(index)} />
+      ))}
+    </div>
+  );
 }
